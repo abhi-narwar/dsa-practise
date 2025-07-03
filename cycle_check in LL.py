0955c75609ca83d061762b82cabac24d623313b3
Abhishek Narwar <142799395+abhisheknarwar0042@users.noreply.github.com>
2025-07-03 16:58:01 +0530

    Update cycle_check in LL.py

--- cycle_check in LL.py
+++ cycle_check in LL.py
@@ -1,33 +1,8 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-class Solution:
-    # Function to check if the linked list has a loop
-    def cycle_check(self, head):                                    '''floyd's cycle detection algorithm--in this question we take two pointer marker1 as as slow pointer which moves one step and marker2 as fast pointer which moves 2 steps ,we iterate over the entire list using while loop  
-        marker1 = head                                               '''
-        marker2 = head
-
-        while marker2 !=None and marker2.next !=None:
-            marker1 = marker1.next
-            marker2 = marker2.next.next
-
-            if marker1 == marker2:
+class Solution:                                          
+    def detectLoop(self, head):
+        slow, fast = head, head      #floyd's cycle detection  
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
                 return True
-
         return False
-
-# Creating nodes
-node1 = Node(1)
-node2 = Node(3)
-node3 = Node(4)
-
-# Linking nodes with a loop
-node1.next = node2
-node2.next= node3
-node3.next = node2  # Loop here
-
-# Creating Solution object and checking for loop
-s = Solution()
-s.cycle_check(node1)  # Output: True
