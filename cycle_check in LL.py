151912b40f101e2bb88ab65eea57831385f64df0
Abhishek Narwar <142799395+abhi-narwar@users.noreply.github.com>
2025-07-29 22:26:20 +0530

    Update cycle_check in LL.py

--- cycle_check in LL.py
+++ cycle_check in LL.py
@@ -1,6 +1,6 @@
 class Solution:                                          
     def detectLoop(self, head):
-        slow, fast = head, head      #floyd's cycle detection  
+        slow, fast = head, head      #floyd's cycle detection
         while fast and fast.next:
             slow, fast = slow.next, fast.next.next
             if slow == fast:
